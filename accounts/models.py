493866839da372e9e7c5214e44d45dc16a93ca24
mrv <lysenko.viktor.83@gmail.com>
2014-04-14 17:42:55 +0300

    make some fix

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Personal_data(models.Model):
-    name = models.CharField(max_length=32, default='')
-    surname = models.CharField(max_length=32, default='')
-    date_of_birth = models.DateField(blank=True)
-    bio = models.TextField(max_length=1024, blank=True, default='')
-    email = models.EmailField(blank=True)
-    jabber = models.EmailField()
-    skype = models.CharField(max_length=32, blank=True, default='')
-    other_contact = models.TextField(max_length=1024, blank=True, default='')
+# class PersonalData(models.Model):
+#     name = models.CharField(max_length=32, default='')
+#     surname = models.CharField(max_length=32, default='')
+#     date_of_birth = models.DateField(blank=True)
+#     bio = models.TextField(max_length=1024, blank=True, default='')
+#     email = models.EmailField(blank=True)
+#     jabber = models.EmailField()
+#     skype = models.CharField(max_length=32, blank=True, default='')
+#     other_contact = models.TextField(max_length=1024, blank=True, default='')
