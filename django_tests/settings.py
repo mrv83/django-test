faa561389f06f39368c29597d952e67876ee0126
mrv <lysenko.viktor.83@gmail.com>
2014-04-18 10:55:26 +0300

    ticket:5 fix MEDIA_ROOT

--- django_tests/settings.py
+++ django_tests/settings.py
@@ -56,8 +56,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-# MEDIA_ROOT = root('django_tests', 'upload')
-MEDIA_ROOT = '/home/mrv/PycharmProjects/django_tests/upload/'
+
+MEDIA_ROOT = root('django_tests', 'upload')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
